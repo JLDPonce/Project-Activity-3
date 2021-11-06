fd8c8f88d7109d2271ea5ad6c434175c6783f456
JLDPonce <89880910+JLDPonce@users.noreply.github.com>
2021-11-06 14:00:50 +0800

    Update project_activity.py

--- project_activity.py
+++ project_activity.py
@@ -2,11 +2,15 @@
 import urllib
 import requests 
 
+#get ip address of the device
 external_ip = urllib.request.urlopen('https://ident.me/').read().decode('utf8')
 
+#json format for the api
 url = "https://ipapi.co/" + external_ip + "/json"
 
+#request data from the api
 json_data = requests.get(url).json()
+
 labels = ["IP Address", "Version", "City", "Region", "Country", "ISP"]
 json_labels = ["ip", "version", "city", "region", "country_name", "org"]
 
